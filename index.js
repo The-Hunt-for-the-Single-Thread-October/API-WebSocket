8d5647881bc676d300f28374890dda2fcff4c591
Jacques <jacques.devuono@gmail.com>
2020-02-13 10:42:01 +0100

    first

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 
-io.on('connection', (socket) => {
-    socket.on('gameCreated', data => {
-        console.log(data);
+io.sockets.on('connection', function(socket) {
+    // once a client has connected, we expect to get a ping from them saying what room they want to join
+    socket.on('create', function() {
+        let roomId = Math.floor((Math.random() * 10000) + 1);
+        socket.join(roomId);
+        io.sockets.in(roomId).emit('message', roomId);
+        console.log(roomId,io.sockets.adapter.rooms[roomId].sockets)
+        console.log("rooms",io.sockets.adapter.rooms)
+
+    });
+
+    socket.on('join', function(room) {
+        socket.join(room);
+        io.sockets.in(room).emit('message', 'c bonn '+room);
+        console.log(room,io.sockets.adapter.rooms[room].sockets)
     });
 });
 
